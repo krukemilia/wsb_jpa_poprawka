9a97b1c85c8c5a9ab45e28a4aff3a2ff926c502d
krukemilia <krukemiliamaria@gmail.com>
2024-06-06 20:32:19 +0200

    dodanie query i naprawienie relacji

--- wsb-jpa1/src/main/java/com/capgemini/wsb/persistence/dao/impl/PatientDaoImpl.java
+++ wsb-jpa1/src/main/java/com/capgemini/wsb/persistence/dao/impl/PatientDaoImpl.java
@@ -2,11 +2,9 @@ package com.capgemini.wsb.persistence.dao.impl;
 
 import com.capgemini.wsb.persistence.dao.PatientDao;
 import com.capgemini.wsb.persistence.entity.PatientEntity;
-import com.capgemini.wsb.persistence.entity.VisitEntity;
 import com.capgemini.wsb.persistence.enums.TreatmentType;
 import org.springframework.stereotype.Repository;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Repository
@@ -16,25 +14,39 @@ public class PatientDaoImpl extends AbstractDao<PatientEntity, Long> implements 
     @Override
     public List<PatientEntity> findByDoctor(String firstName, String lastName) { // TODO - napisac query
 
-        return new ArrayList<>();
+        String query = "SELECT patient FROM PatientEntity patient JOIN patient.visits visit WHERE visit.doctor.firstName = :firstName AND visit.doctor.lastName = :lastName";
+        return entityManager.createQuery(query, PatientEntity.class)
+                .setParameter("firstName", firstName)
+                .setParameter("lastName", lastName)
+                .getResultList();
 
     }
 
     @Override
     public List<PatientEntity> findPatientsHavingTreatmentType(TreatmentType treatmentType) { // TODO - napisac query
-        return new ArrayList<>();
+        String query = "SELECT DISTINCT patient FROM PatientEntity patient JOIN patient.visits visit JOIN visit.medicalTreatments medicalTreatment WHERE medicalTreatment.type=:treatmentType";
+        return entityManager.createQuery(query, PatientEntity.class)
+                .setParameter("treatmentType", treatmentType)
+                .getResultList();
     }
 
     @Override
     public List<PatientEntity> findPatientsSharingSameLocationWithDoc(String firstName, String lastName) { // TODO - napisac query
 
-        return new ArrayList<>();
+        String query = "SELECT DISTINCT patient FROM PatientEntity patient " +
+                "JOIN patient.addresses patientAddress, DoctorEntity doctor JOIN doctor.addresses doctorAddress" +
+                " WHERE doctor.firstName=:firstName AND doctor.lastName=:lastName AND patientAddress = doctorAddress";
+        return entityManager.createQuery(query, PatientEntity.class)
+                .setParameter("firstName", firstName)
+                .setParameter("lastName", lastName).getResultList();
+
 
     }
 
     @Override
     public List<PatientEntity> findPatientsWithoutLocation() { // TODO - napisac query
 
-        return null;
+        String query = "SELECT patient FROM PatientEntity patient WHERE patient.addresses IS EMPTY";
+        return entityManager.createQuery(query, PatientEntity.class).getResultList();
     }
 }
